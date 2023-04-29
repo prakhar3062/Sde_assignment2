72c0831809f2eb40cd43e935e6b2492c7588fbbd
prakhar3062 <prakharm.bt.20@nitj.ac.in>
2023-04-29 15:46:18 +0530

    Done

---
+++ src/Components/Comparison.js
@@ -0,0 +1,85 @@
+import './style2.css';
+import React from 'react';
+function Comparsion(){
+  return(  
+  <div className='body'>
+   <div className='header m-4 p-4'>
+      <h1 >Study India vs Study abroad</h1>
+   </div>
+   <div className="my_container container p-4 ">
+      <div className=" my_row row ">
+        <div className='my_col_header col-sm-3 p-4 '>
+          <div className='row  border-bottom border-dark'>
+            <p className='p_header fs-3'>row 1</p>
+          </div>
+          <div className='row p-3 border-bottom border-dark'>
+              row 2
+          </div>
+          <div className='row p-3 border-bottom border-dark'>
+              row 3
+          </div>
+          <div className='row p-3 border-bottom border-dark'>
+              row 4
+          </div>
+          <div className='row p-3 border-bottom border-dark'>
+              row 5
+          </div>
+          <div className='row p-3 border-bottom border-dark'>
+              row 6
+          </div>
+          <div className='row p-3 '>
+              row 1
+          </div>
+        </div>
+        <div className="my_col col-4 p-4">
+        <div className='row  border-bottom border-dark'>
+            <p className='p_header fs-3'>row 1</p>
+          </div>
+          <div className='row p-3 border-bottom border-dark'>
+              row 2
+          </div>
+          <div className='row p-3 border-bottom border-dark'>
+              row 3
+          </div>
+          <div className='row p-3 border-bottom border-dark'>
+              row 4
+          </div>
+          <div className='row p-3 border-bottom border-dark'>
+              row 5
+          </div>
+          <div className='row p-3 border-bottom border-dark'>
+              row 6
+          </div>
+          <div className='row p-3 '>
+              row 1
+          </div>
+        </div>
+        <div className="my_col col-4 p-4">
+        <div className='row  border-bottom border-dark'>
+            <p className='p_header fs-3'>row 1</p>
+          </div>
+          <div className='row p-3 border-bottom border-dark'>
+              row 2
+          </div>
+          <div className='row p-3 border-bottom border-dark'>
+              row 3
+          </div>
+          <div className='row p-3 border-bottom border-dark'>
+              row 4
+          </div>
+          <div className='row p-3 border-bottom border-dark'>
+              row 5
+          </div>
+          <div className='row p-3 border-bottom border-dark'>
+              row 6
+          </div>
+          <div className='row p-3 '>
+              row 1
+          </div>
+        </div>
+        </div>
+   </div>
+  </div>
+  );
+}
+export default Comparsion;
---
+++ src/Components/Component2.js
@@ -0,0 +1,113 @@
+import React from 'react';
+
+function Component2() {
+  return (
+    <div>
+      <header className="m-3 p-3 border-bottom">
+        <h1 className="text-center m-4">Highlights About India</h1>
+
+      </header>
+      <div className="content align-middle m-4">
+      <div className="row mt-4 justify-content-center">
+        <div className="col-4 text-black">
+          <div className="card mb-3">
+            <div className="row g-0">
+              <div className="col-md-8">
+                <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">This is a wider card </p>
+                </div>
+              </div>
+              <div className="col-md-4">
+                <img src="..." className="img-fluid rounded-start" alt="..."/>
+              </div>
+            </div>      
+          </div>
+        </div>  
+        <div className="col-4 text-black">
+          <div className="card mb-3">
+              <div className="row g-0">
+                <div class="col-md-4">
+                  <img src="..." className="img-fluid rounded-start" alt="..."/>
+                </div>
+                <div className="col-md-8">
+                  <div className="card-body">
+                  <h5 className="card-title">Card title</h5> 
+                  <p className="card-text">This is a wider card </p>
+                  </div>
+                </div>
+              </div>      
+            </div>
+          </div>
+        </div>
+      <div className="row justify-content-center">
+        <div className="col-4 text-black">
+          <div className="card mb-3">
+            <div className="row g-0">
+              <div className="col-md-8">
+                <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">This is a wider card </p>
+                </div>
+              </div>
+              <div className="col-md-4">
+                <img src="..." className="img-fluid rounded-start" alt="..."/>
+              </div> 
+            </div>      
+          </div>
+        </div>
+        <div className="col-4 text-black">
+          <div className="card mb-3">
+            <div className="row g-0">
+              <div className="col-md-4">
+                <img src="..." className="img-fluid rounded-start" alt="..."/>
+              </div>
+              <div className="col-md-8">
+                <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">This is a wider card</p>
+                </div>
+              </div>
+            </div>      
+          </div>
+        </div>
+      </div>
+      <div className="row justify-content-center">
+        <div className="col-4 text-black">
+          <div className="card mb-3">
+            <div className="row g-0">
+              <div className="col-md-8">
+                <div className="card-body">
+                <h5 className="card-title">Card title</h5>  
+                <p className="card-text">This is a wider card</p>
+                </div>
+              </div>
+              <div className="col-md-4">
+                <img src="..." className="img-fluid rounded-start" alt="..."/>
+              </div>
+            </div>      
+          </div>
+        </div>
+        <div className="col-4 text-black">
+          <div className="card mb-3">
+            <div className="row g-0">
+              <div className="col-md-4">
+                <img src="..." className="img-fluid rounded-start" alt="..."/>
+              </div>
+              <div className="col-md-8">
+                <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">This is a wider card</p>
+                </div>
+              </div>
+            </div>      
+          </div>
+        </div>
+      </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default Component2;
